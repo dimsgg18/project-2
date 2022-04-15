aaf02e5a8fb26c74b0ad23486c9f98b4fee96520
dimsgg18 <dimasgalangajiwiguna@gmail.com>
2022-04-15 12:14:10 +0700

    menambahkan dashboard dan sidebar

---
+++ script/dashboard.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("sidebar").style.width = "30%";
+    document.getElementById("main").style.marginLeft ="30%";
+
+}
+
+function closeNav(){
+    document.getElementById(`sidebar`).style.width = `0%`;
+    document.getElementById(`main`).style.marginLeft = `0%`;
+}
