e1fe2b3ae76c433c2a00ad907fe016c8acc8a162
dimsgg18 <dimasgalangajiwiguna@gmail.com>
2022-04-15 15:09:22 +0700

    menambahkan bagian navbar

--- script/dashboard.js
+++ script/dashboard.js
@@ -8,3 +8,41 @@ function closeNav(){
     document.getElementById(`sidebar`).style.width = `0%`;
     document.getElementById(`main`).style.marginLeft = `0%`;
 }
+
+// fungsi dropdown 1
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+//   fungsi dropdown 2
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn.')) {
+      var dropdowns = document.getElementsByClassName("dropdown-time");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
