4e3d43270d37bf28a2e34b5c1e2bd07257a94caa
dimsgg18 <dimasgalangajiwiguna@gmail.com>
2022-04-16 11:41:57 +0700

    menambah bagian bagian add book pop up

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,12 +1,12 @@
 function openNav(){
     document.getElementById("sidebar").style.width = "30%";
-    document.getElementById("main").style.marginLeft ="30%";
+
 
 }
 
 function closeNav(){
-    document.getElementById(`sidebar`).style.width = `0%`;
-    document.getElementById(`main`).style.marginLeft = `0%`;
+    document.getElementById(`sidebar`).style.width = `0`;
+
 }
 
 // fungsi dropdown 1
@@ -16,7 +16,7 @@ function myFunction() {
   }
 
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches(`.dropbtn-categories`)) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -35,7 +35,7 @@ function myFunction2() {
   }
 
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn.')) {
+    if (!event.target.matches(`.dropbtn-time`)) {
       var dropdowns = document.getElementsByClassName("dropdown-time");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -46,3 +46,32 @@ function myFunction2() {
       }
     }
   }
+
+
+  // bagian fungsi modal pop up
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
